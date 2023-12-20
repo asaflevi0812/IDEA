739007899a8d5c6be5ae7ee8eff30c03969c7cbc
Asaf Levi <asaf.levi@csf.technion.ac.il>
2023-12-20 21:09:23 +0200

    Added initial scripts

---
+++ scripts/clear_cache.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo bash -c "echo 1 > /proc/sys/vm/drop_caches"
---
+++ scripts/figure_10.sh
@@ -0,0 +1,17 @@
+cp scripts/lookup_title.txt lookup.log
+scripts/clear_cache.sh
+destor -m -f"keywords/linux/16/file-med.txt"
+scripts/clear_cache.sh
+destor -l -f"keywords/linux/16/file-med.txt"
+scripts/clear_cache.sh
+destor -m -f"keywords/linux/32/file-med.txt"
+scripts/clear_cache.sh
+destor -l -f"keywords/linux/32/file-med.txt"
+scripts/clear_cache.sh
+destor -m -f"keywords/linux/64/file-med.txt"
+scripts/clear_cache.sh
+destor -l -f"keywords/linux/64/file-med.txt"
+scripts/clear_cache.sh
+destor -m -f"keywords/linux/128/file-med.txt"
+scripts/clear_cache.sh
+destor -l -f"keywords/linux/128/file-med.txt"
