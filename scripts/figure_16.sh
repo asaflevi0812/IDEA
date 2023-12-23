3846f2390302e1e09825646611d0aef66cf7182d
Asaf Levi <asaf.levi@csf.technion.ac.il>
2023-12-23 14:59:52 +0200

    Added figure 16 script

---
+++ scripts/figure_16.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+cp scripts/lookup_title.txt lookup.log
+scripts/clear_cache.sh
+destor -m -f"keywords/linux/1a/file-med.txt"
+scripts/clear_cache.sh
+destor -l -f"keywords/linux/1a/file-med.txt"
+scripts/clear_cache.sh
+destor -m -f"keywords/linux/1b/file-med.txt"
+scripts/clear_cache.sh
+destor -l -f"keywords/linux/1b/file-med.txt"
+scripts/clear_cache.sh
+destor -m -f"keywords/linux/1c/file-med.txt"
+scripts/clear_cache.sh
+destor -l -f"keywords/linux/1c/file-med.txt"
+scripts/clear_cache.sh
+destor -m -f"keywords/linux/1d/file-med.txt"
+scripts/clear_cache.sh
+destor -l -f"keywords/linux/1d/file-med.txt"
+scripts/clear_cache.sh
+destor -m -f"keywords/linux/1a/chunk-med.txt"
+scripts/clear_cache.sh
+destor -l -f"keywords/linux/1a/chunk-med.txt"
+scripts/clear_cache.sh
+destor -m -f"keywords/linux/1b/chunk-med.txt"
+scripts/clear_cache.sh
+destor -l -f"keywords/linux/1b/chunk-med.txt"
+scripts/clear_cache.sh
+destor -m -f"keywords/linux/1c/chunk-med.txt"
+scripts/clear_cache.sh
+destor -l -f"keywords/linux/1c/chunk-med.txt"
+scripts/clear_cache.sh
+destor -m -f"keywords/linux/1d/chunk-med.txt"
+scripts/clear_cache.sh
+destor -l -f"keywords/linux/1d/chunk-med.txt"
