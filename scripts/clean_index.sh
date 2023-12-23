cb9e05a522e5abb4e81635d916cb89dd6a91158e
Asaf Levi <asaf.levi@csf.technion.ac.il>
2023-12-23 20:40:07 +0200

    fixed awkward deletion

--- scripts/clean_index.sh
+++ scripts/clean_index.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while [[ $(findmnt working_directories) != "" ]]; do sudo umount working_directories; done
-rm -rf working_directories/*
+rm -rf working_directories/index_directory/* working_directories/chunk_to_file_directory/*
 mkdir -p working_directories/index_directory working_directories/chunk_to_file_directory
 mkdir -p working_directories/backup_directory
